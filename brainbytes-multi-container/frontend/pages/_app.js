6455f47923e648f345be14e85a9971b5021fdbef
nicolecoloma <138119129+nicolecoloma@users.noreply.github.com>
2026-07-10 15:05:37 +0300

    Create _app.js

---
+++ brainbytes-multi-container/frontend/pages/_app.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
